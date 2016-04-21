8699e16ef288f88a70edcc259391a1c233415ab9
khofmans <kris.hofmans@gmail.com>
2016-04-21 21:01:45 +0200

    spring boot web module

--- web/src/main/java/be/jpendel/Application.java
+++ web/src/main/java/be/jpendel/Application.java
@@ -1,7 +1,9 @@
 package be.jpendel;
 
+import be.jpendel.application.CreateEventCommand;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class Application {
@@ -9,4 +11,5 @@ public class Application {
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
+
 }
