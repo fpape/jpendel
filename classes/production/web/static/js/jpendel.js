1b26f18f24acb81b28ca2a4661373b71cc9d87fa
Guido Dechamps <Guido.Dechamps@solid-it.biz>
2016-07-26 22:58:50 +0200

    removed checkin classes dir

--- classes/production/web/static/js/jpendel.js
+++
@@ -1,6 +0,0 @@
-angular.module('jpendel', [])
-    .controller('eventController', function ($http, $scope) {
-        $http.get('/event').then(function (response) {
-            $scope.events = response.data;
-        })
-    });
