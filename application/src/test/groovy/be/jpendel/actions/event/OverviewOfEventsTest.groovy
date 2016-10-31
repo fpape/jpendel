667609cd3fcadd6ef8eea8dd1093da1ba398b882
Frederic Pape <frederic.pape@axians.com>
2016-10-31 21:42:57 +0100

    test polishing

--- application/src/test/groovy/be/jpendel/actions/event/OverviewOfEventsTest.groovy
+++ application/src/test/groovy/be/jpendel/actions/event/OverviewOfEventsTest.groovy
@@ -8,28 +8,34 @@ import spock.lang.Subject
 import java.time.LocalDateTime
 
 class OverviewOfEventsTest extends Specification {
-    def mockEventRepository = Mock(EventRepository) {
-        getAll() >> makeEvents()
-    }
+    def mockEventRepository = Mock(EventRepository)
+//    EventRepository mockEventRepository = Mock()
+
     @Subject
     def underTest = OverviewOfEvents.with(mockEventRepository)
 
     List<Event> makeEvents() {
         (0..9).collect {
             Event.newBuilder()
-                    .withLocation("location" + it)
-                    .withName("name" + it)
+                    .withLocation("location: $it")
+                    .withName("name: $it")
                     .withStartDateTime(LocalDateTime.now())
                     .build()
         }
     }
 
     def "Overview"() {
+        given:
+        mockEventRepository.getAll() >> makeEvents()
         when:
         def result = underTest.overview()
         then:
         result.size() == 10
-
+        result.every { it.location.startsWith('location: ') }
+        result.every { it.location ==~ /location: \d/ }
+        result.every { it.name.startsWith('name: ') }
+        result.every { it.name ==~ /name: \d/ }
+        result.every { it.startDateTime }
     }
 
 }
